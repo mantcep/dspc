945efa9f808927eecb1bc73628e8da06443ad248
Mantas Cepulkovskis <mantas.cepulkovskis@gmail.com>
2020-02-16 17:29:40 +0200

    Finish preparatory assignment

---
+++ utils.py
@@ -0,0 +1,79 @@
+import requests
+import pandas as pd
+import numpy as np
+
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+VERSION = ''
+LIMIT = 50
+
+
+def get_nearby_venues(names, latitudes, longitudes, radius=500):
+    """Reusing function provided in one of the Data Science Professional Certificate lab assignments"""
+    venues_list = []
+    for name, lat, lng in zip(names, latitudes, longitudes):
+        print(name)
+
+        # create the API request URL
+        url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
+            CLIENT_ID,
+            CLIENT_SECRET,
+            VERSION,
+            lat,
+            lng,
+            radius,
+            LIMIT)
+
+        # make the GET request
+        results = requests.get(url).json()["response"]['groups'][0]['items']
+
+        # return only relevant information for each nearby venue
+        venues_list.append([(
+            name,
+            lat,
+            lng,
+            v['venue']['name'],
+            v['venue']['location']['lat'],
+            v['venue']['location']['lng'],
+            v['venue']['categories'][0]['name']) for v in results])
+
+    nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
+    nearby_venues.columns = ['Neighborhood',
+                             'Neighborhood Latitude',
+                             'Neighborhood Longitude',
+                             'Venue',
+                             'Venue Latitude',
+                             'Venue Longitude',
+                             'Venue Category']
+
+    return nearby_venues
+
+
+def return_most_common_venues(row, num_top_venues):
+    """Reusing function provided in one of the Data Science Professional Certificate lab assignments"""
+    row_categories = row.iloc[1:]
+    row_categories_sorted = row_categories.sort_values(ascending=False)
+
+    return row_categories_sorted.index.values[0:num_top_venues]
+
+
+def sort_neighborhood_venues(df, num_top_venues=10):
+    """Reusing code provided in one of the Data Science Professional Certificate lab assignments for this function"""
+    indicators = ['st', 'nd', 'rd']
+
+    # Create columns according to number of top venues
+    columns = ['Neighborhood']
+    for ind in np.arange(num_top_venues):
+        try:
+            columns.append('{}{} Most Common Venue'.format(ind+1, indicators[ind]))
+        except:
+            columns.append('{}th Most Common Venue'.format(ind+1))
+
+    # Create a new dataframe
+    neighborhoods_venues_sorted = pd.DataFrame(columns=columns)
+    neighborhoods_venues_sorted['Neighborhood'] = df['Neighborhood']
+
+    for ind in np.arange(df.shape[0]):
+        neighborhoods_venues_sorted.iloc[ind, 1:] = return_most_common_venues(df.iloc[ind, :], num_top_venues)
+
+    return neighborhoods_venues_sorted
